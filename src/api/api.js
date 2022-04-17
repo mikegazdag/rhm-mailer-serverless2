7d7b52d8a854f2d1ffe3cadf4a3cd94047de4005
Mike Gazdag <7515813+mikegazdag@users.noreply.github.com>
2022-04-16 23:51:09 -0700

    Add nodemon and single entry point

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,13 @@
-module.exports.handler = (evt, ctx, done) => {
-  done(null, {
-    statusCode: 200,
-    headers: {},
-    body: JSON.stringify({ message: "hello" }),
-  });
-};
+const express = require("express");
+const app = express();
 
-// const express = require("express");
-// const app = express();
+app.get("/contact/create", (req, res) => {
+  console.log("reps", resp);
+  res.json({ statusCode: 200, message: resp || "not found" });
 
-// app.get("/test", (req, res) => {
-//   res.json({ message: "ok" });
-// });
+  // 1. Validate response
+  // 2. Render template
+  // 3. Send email bodies
+});
 
-// module.exports = app;
+module.exports = app;
